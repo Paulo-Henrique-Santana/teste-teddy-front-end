7c84491c051890c28a7e8ba197a82c2ce7dbf2fb
Paulo-Henrique-Santana <phenrique.santana@hotmail.com>
2024-11-27 14:27:31 -0300

    feat: adicionar página de clientes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
+import { ClientesComponent } from './pages/clientes/clientes.component';
 import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'clientes', component: ClientesComponent }
 ];
