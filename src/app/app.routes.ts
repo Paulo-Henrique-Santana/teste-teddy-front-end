06e1bda75ba034de766d62a3cfb9cda6355d49c8
Paulo-Henrique-Santana <phenrique.santana@hotmail.com>
2024-11-27 20:32:32 -0300

    feat: remover cabeçalho da tela inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
 import { ClientesComponent } from './pages/clientes/clientes.component';
 import { HomeComponent } from './pages/home/home.component';
+import { MainComponent } from './pages/main/main.component';
 
 export const routes: Routes = [
+  {
+    path: 'main',
+    component: MainComponent,
+    children: [
+      { path: 'clientes', component: ClientesComponent },
+    ],
+  },
   { path: '', component: HomeComponent },
-  { path: 'clientes', component: ClientesComponent }
 ];
